1fbb0b230789378d044dcee60d7c6547081df134
Ozil <126558736+OZ-astra@users.noreply.github.com>
2025-05-24 11:33:06 +0100

    Update fixed bugs generatedTicket.js

--- generatedTicket.js
+++ generatedTicket.js
@@ -1,14 +1,15 @@
 /** @format */
+const storedUserData = JSON.parse(sessionStorage.getItem("userData"));
+
+if (!storedUserData) {
+	window.location.href = "index.html";
+}
+
 const personFullname = document.querySelectorAll(".person-name");
 const personEmail = document.getElementById("person-email");
 const dateOfRegistration = document.getElementById("date-generated");
 const github = document.getElementById("github-name");
 document.addEventListener("DOMContentLoaded", () => {
-	if (!localStorage.getItem("userData") === "") {
-		const storedUserData = JSON.parse(localStorage.getItem("userData"));
-	} else {
-		window.location.href = "index.html";
-	}
 	personFullname.forEach((name) => {
 		name.textContent = storedUserData.username;
 	});
@@ -40,5 +41,4 @@ document.addEventListener("DOMContentLoaded", () => {
 	document.querySelector(".inputs").prepend(img);
 
 	github.textContent = storedUserData.githubName;
-	console.log(storedUserData.githubName);
 });
